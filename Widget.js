ab911df8b155bc7794cef8bda78698c32feb16d1
Mónica <wanderlux@users.noreply.github.com>
2018-10-18 13:25:24 +0200

    Update Widget.js

--- Widget.js
+++ Widget.js
@@ -1,17 +1,15 @@
 define([
   'dojo/_base/declare',
   'jimu/BaseWidget',
-  './widgets/thirdparty-wab/libs/clipboard.min.js'
+  './widgets/thirdparty-wab/libs/clipboard.min.js' // load it
 ],
 
-function(declare, BaseWidget, ClipboardJS) {
+function(declare, BaseWidget, ClipboardJS) { // name it
 
   return declare([BaseWidget], {
 
-    baseClass: 'jimu-thirdparty-wab', //para el css
-
     startup: function() {
-      new ClipboardJS('.btn');
+      new ClipboardJS('.btn'); // use it
     }
   });
 });
