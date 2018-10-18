ec87e1b4948af397ddd7a9b4f00eccd0ef7e6aa5
Mónica Monteagudo <wanderlux@users.noreply.github.com>
2018-10-18 12:51:54 +0200

    add clipboardjs

--- Widget.js
+++ Widget.js
@@ -1,26 +1,17 @@
 define([
   'dojo/_base/declare',
   'jimu/BaseWidget',
-  './widgets/thirdparty/libs/proj4.js',
-  "esri/geometry/Extent",
-  "esri/SpatialReference",
-  'esri/geometry/GeographicTransformation',
-  'esri/geometry/projection',
-  'jimu/loaderplugins/order-loader!./widgets/BuscadorICV/libs/jquery-1-11-1.min.js',
-  'jimu/loaderplugins/order-loader!./widgets/BuscadorICV/libs/jquery-ui.min.js',
+  './widgets/thirdparty-wab/libs/clipboard.min.js'
 ],
 
-function(declare, BaseWidget, proj4, Extent, SpatialReference, GeographicTransformation, projection) {
+function(declare, BaseWidget, ClipboardJS) {
 
   return declare([BaseWidget], {
 
-    baseClass: 'jimu-widget-buscadoricv', //para el css
-    
+    baseClass: 'jimu-thirdparty-wab', //para el css
+
     startup: function() {
+      new ClipboardJS('.btn');
     }
   });
-
 });
-    
-    
-    
